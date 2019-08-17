b4f8d104015da8c8c96255e3a257e9c64fd7894b
Harshal <harshal95iitk@gmail.com>
2019-08-17 23:56:55 +0530

    Matrix manipulation using NumPy

---
+++ matrix_manipulation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# matrix initialization
+x = np.array([1, 2, 3, 4])
+y = np.array([5, 6, 7, 8])
+print('x =',x)
+print('y =', y)
+# add() is used to add matrices
+print('Addition of x and y:', np.add(x, y))
+print('x + y =', x+y)
